5933d757579968214b26030e3eae3110db5978b7
fernandalpez3 <ma.fernandalpez@gmail.com>
2017-11-01 11:00:31 -0600

    Add files via upload

---
+++ main/main/java/com/example/fernandalopezcardenas/uneatfinal/DetailPlate.java
@@ -0,0 +1,54 @@
+package com.example.fernandalopezcardenas.uneatfinal;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Created by fernandalopezcardenas on 20/09/17.
+ */
+
+public class DetailPlate implements Serializable{
+    String Name;
+    int Price;
+    String UID;
+    ArrayList<String> Ingredients;
+
+    public DetailPlate(){
+
+    }
+
+    public DetailPlate(String Name, int Price, ArrayList<String> Ingredients, String UID) {
+        this.Name = Name;
+        this.Price = Price;
+        this.UID=UID;
+        this.Ingredients = Ingredients;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    public int getPrice() {
+        return Price;
+    }
+    public void setPrice(int Price) {
+        this.Price = Price;
+    }
+    public ArrayList<String> getIngredients() {return Ingredients;}
+    public void setIngredients(ArrayList<String> Ingredients) {
+        this.Ingredients = Ingredients;
+    }
+    public String getUID() {
+        return UID;
+    }
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
